91b55dc07ded5e673a8dd059549016531b443d29
yaljamal <yazan.aljamal@gmail.com>
2020-06-13 01:24:22 +0300

    run the server

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+require('dotenv').config();
+const mongoose=require('mongoose');
+const server=require('./lib/server.js');
+
+// mongoose.connect(process.env.MONGODB_URI,{
+//   useNewUrlParser:true,
+//   useUnifiedTopology:true,
+// });
+
+server.start(process.env.PORT);
